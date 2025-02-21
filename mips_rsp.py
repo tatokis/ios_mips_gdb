ddb96fe69dc6d40f5113c8c75f3d2c98e43c1feb
Tasos Sahanidis <tasos@tasossah.com>
2025-02-21 23:59:22 +0200

    Partially port to Python3

--- mips_rsp.py
+++ mips_rsp.py
@@ -33,11 +33,11 @@ aslr_offset = None
 
 isSerial = True
 
-for k, v in reg_map.iteritems():
+for k, v in reg_map.items():
     reg_map_rev[v] = k
 
 if len(sys.argv) < 2:
-    print 'Specify serial device as a parameter'
+    print('Specify serial device as a parameter')
     sys.exit(1)
 
 ser = serial.Serial(
@@ -73,7 +73,7 @@ def isValidDword(hexdword):
     if len(hexdword) != 8:
         return False
     try:
-        hexdword.decode('hex')
+        bytes.fromhex(hexdword)
     except TypeError:
         return False
     return True
@@ -99,13 +99,13 @@ def decodeRLE(data):
                 reply = reply + data[i-1]
             i = i + 3
         if data[i] == "#":
-            break   
+            break
         reply = reply + data[i]
         i = i + 1
     return reply
 
 def print_help():
-    print '''Command reference:
+    print('''Command reference:
 c                           - continue program execution
 stepi                       - step into
 nexti                       - step over
@@ -122,7 +122,7 @@ disas <addr> [aslr]         - disassemble at address. Optional "aslr" parameter 
 set_aslr_offset             - set aslr offset for code section
 
 you can also manually send any GDB RSP command
-    '''
+    ''')
 
 def CreateGetMemoryReq(address, len):
     address = "m" + address + "," + len
@@ -139,32 +139,31 @@ def DisplayRegistersMIPS(regbuffer):
 
 def GdbCommand(command):
     global isSerial
-    logger.debug('GdbCommand sending: {}'.format(checksum(command))) 
-    
-    ser.write('{}'.format(checksum(command)))
+    logger.debug('GdbCommand sending: {}'.format(checksum(command)))
+
+    ser.write(('{}'.format(checksum(command))).encode())
     if command == 'c':
         return ''
     out = ''
-    char =''
+    char = ''
     while char != "#":
-        char = ser.read(1)     
-        out = out + char    
-    ser.read(2)            
+        char = ser.read(1).decode()
+        out = out + char
+    ser.read(2)
 
     logger.debug('Raw output from cisco: {}'.format(out))
     newrle = decodeRLE(out)
-    decoded = newrle.decode()
-    while decoded[0] == "|" or decoded[0] == "+" or decoded[0] == "$":
-        decoded = decoded[1:]
-    return decoded    
+    while newrle[0] == "|" or newrle[0] == "+" or newrle[0] == "$":
+        newrle = newrle[1:]
+    return bytes.fromhex(newrle)
 
 def OnReadReg():
     regs =  DisplayRegistersMIPS(GdbCommand('g'))
-    print 'All registers:'
-    for k, reg_name in reg_map.iteritems():
-        print "{}: {}".format(reg_name, regs[reg_map_rev[reg_name]])
-    print 'Control registers:'
-    print "PC: {} SP: {} RA: {}".format(regs[reg_map_rev['pc']],regs[reg_map_rev['sp']], regs[reg_map_rev['ra']])
+    print('All registers:')
+    for k, reg_name in reg_map.items():
+        print("{}: {}".format(reg_name, regs[reg_map_rev[reg_name]]))
+    print('Control registers:')
+    print("PC: {} SP: {} RA: {}".format(regs[reg_map_rev['pc']],regs[reg_map_rev['sp']], regs[reg_map_rev['ra']]))
     return regs
 
 def OnWriteReg(command):
@@ -208,10 +207,10 @@ def OnWriteMem(addr, data):
         return None
     
 def hex2int(s):
-    return unpack(">I", s.decode('hex'))[0]
+    return unpack(">I", bytes.fromhex(s))[0]
 
 def int2hex(num):
-    return pack(">I", num & 0xffffffff).encode('hex')
+    return pack(">I", num & 0xffffffff).hex()
 
 def OnBreak(command):
     global breakpoints
@@ -247,10 +246,10 @@ def OnDelBreak(command):
     (_, b_num) = command.rstrip().split(' ')
     logger.debug('OnDelBreak')
     item_to_delete = None
-    for k, v in breakpoints.iteritems():
+    for k, v in breakpoints.items():
         try:
             if v[0] == int(b_num):
-                res = OnWriteMem(k, v[1]) 
+                res = OnWriteMem(k, v[1])
                 if res:
                     item_to_delete = k
                     break
@@ -273,21 +272,21 @@ def OnSearchMem(addr, pattern):
         mem = GdbCommand('m{},00c7'.format(cur_addr))
         buf += mem
         if i %1000 == 0:
-            print  cur_addr
-            print hexdump(mem.decode('hex'))
+            print(cur_addr)
+            print(hexdump(mem.decode('hex')))
         if pattern in buf[-100:-1]:
-            print 'FOUND at {}'.format(cur_addr)
+            print('FOUND at {}'.format(cur_addr))
             return
-        cur_addr = pack(">I", unpack(">I",cur_addr.decode('hex'))[0] + 0xc7).encode('hex')
+        cur_addr = pack(">I", unpack(">I",cur_addr.decode('hex'))[0] + 0xc7).hex()
 
 def OnListBreak():
     global breakpoints
     global breakpoints_count
-    for k, v in breakpoints.iteritems():
-        print '{}: {}'.format(v[0], k)
+    for k, v in breakpoints.items():
+        print('{}: {}'.format(v[0], k))
 
 def OnStepInto():
-    ser.write("$s#73\r\n")
+    ser.write("$s#73\r\n".encode())
     ser.read(5)
     OnReadReg()
     OnDisas('disas')
@@ -295,24 +294,24 @@ def OnStepInto():
 def OnNext():
     regs = OnReadReg()
     pc = unpack('>I', regs[reg_map_rev['pc']].decode('hex'))[0]
-    pc_after_branch = pc + 8 
-    pc_in_hex = pack('>I', pc_after_branch).encode('hex')
+    pc_after_branch = pc + 8
+    pc_in_hex = pack('>I', pc_after_branch).hex()
     OnBreak('break {}'.format(pc_in_hex))
     GdbCommand('c')
     OnReadReg()
-    OnDelBreak('del {}'.format(breakpoints[pc_in_hex][0])) 
+    OnDelBreak('del {}'.format(breakpoints[pc_in_hex][0]))
 
 def OnDumpMemory(start, stop):
-    buf = ''
-    print start, stop
+    buf = b''
+    print(start, stop)
     if not isValidDword(start) or not isValidDword(stop):
         logger.error('Invalid memory range specified')
-        return 
+        return
     cur_addr = start
-    while unpack(">I",cur_addr.decode('hex'))[0] < unpack(">I", stop.decode('hex'))[0]:
+    while unpack(">I",bytes.fromhex(cur_addr))[0] < unpack(">I", bytes.fromhex(stop))[0]:
         res = GdbCommand('m{},00c7'.format(cur_addr))
         logger.info('Dumping at {} len {}'.format(cur_addr, len(res)))
-        cur_addr = pack(">I", unpack(">I",cur_addr.decode('hex'))[0] + 0xc7).encode('hex')
+        cur_addr = pack(">I", unpack(">I",bytes.fromhex(cur_addr))[0] + 0xc7).hex()
         buf += res
     return buf
 
@@ -327,30 +326,30 @@ def OnDisas(command):
 
     regs =  DisplayRegistersMIPS(GdbCommand('g'))
     pc = hex2int(regs[reg_map_rev['pc']])
-    
+
     for lexem in lex[1:]:
         if lexem != 'aslr':
             if not isValidDword(lexem):
                 logger.error('Invalid address supplied')
                 return
-            pc = hex2int(lexem) 
+            pc = hex2int(lexem)
 
     logger.debug('OnDisas PC = {}'.format(pc))
     buf = OnReadMem(int2hex(pc - 20 * 4), 40 * 4)
     md = cs.Cs(cs.CS_ARCH_MIPS, cs.CS_MODE_MIPS32 | cs.CS_MODE_BIG_ENDIAN)
-    
+
     if len(lex) > 1:
         if lex[1] == 'aslr' and aslr_offset != None:
             pc -= aslr_offset
 
     for i in md.disasm(buf.decode('hex'), pc - 20 * 4):
         color = 'green' if i.address == pc else 'blue'
-        print("0x%x:\t%s\t%s" %(i.address, colored(i.mnemonic, color), colored(i.op_str, color)))
-                
+        print(("0x%x:\t%s\t%s" %(i.address, colored(i.mnemonic, color), colored(i.op_str, color))))
+
 
 while True:
     try:
-        command = raw_input('> command: ').rstrip()
+        command = input('> command: ').rstrip()
         if command == 'exit':
             sys.exit(0)
         elif command == 'help':
@@ -375,16 +374,16 @@ while True:
             _, start, length = command.split(' ')
             buf = OnReadMem(start, int(length))
             for line in hexdump_gen(buf.decode('hex'), base_addr=hex2int(start), sep=' '):
-                print line
+                print(line)
         elif command.startswith('write'):
             _, dest, value = command.split(' ')
             value.decode('hex')
             OnWriteMem(dest, value)
         elif command.startswith('search'):
-            _, addr, pattern = command.split(' ') 
+            _, addr, pattern = command.split(' ')
             OnSearchMem(addr, pattern)
         elif command.startswith('gdb kernel'):
-            ser.write('{}\n'.format('gdb kernel'))
+            ser.write(('{}\n'.format('gdb kernel')).encode())
         elif command.startswith('dump'):
             _, start, stop = command.split(' ')
             buf = OnDumpMemory(start.lower(), stop.lower())
@@ -400,10 +399,10 @@ while True:
             OnDisas(command)
         else:
 
-            ans = raw_input('Command not recognized.\nDo you want to send raw command: {} ? [yes]'.format(checksum(command.rstrip())))
-            if ans == '' or ans == 'yes': 
+            ans = input('Command not recognized.\nDo you want to send raw command: {} ? [yes]'.format(checksum(command.rstrip())))
+            if ans == '' or ans == 'yes':
                 reply = GdbCommand(command.rstrip())
-                print 'Cisco response:', reply.rstrip()
+                print('Cisco response:', reply.rstrip())
     except (KeyboardInterrupt, serial.serialutil.SerialException, ValueError, TypeError) as e:
-        print '\n{}'.format(e)
-        print 'Type "exit" to end debugging session'
+        print('\n{}'.format(e))
+        print('Type "exit" to end debugging session')
